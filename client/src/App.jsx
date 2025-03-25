1da11a6dd4db4325ec13bebbc594932a732b536b
Huỳnh Phạm Long Triều <trieu7a2005@gmail.com>
2025-03-25 14:21:48 +0700

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  
+  return (
+    <section className=" h-screen bg-linear-to-t from-sky-500 to-indigo-500">
+      <p className=" text-white text-2xl font-semibold">hello world</p>
+    </section>
+  )
+}
+
+export default App
